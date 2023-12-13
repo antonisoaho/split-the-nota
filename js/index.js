75a673942f3dbe640e8f417ad0f7bffa2f7997fc
antonisoaho <isoahoanton@gmail.com>
2023-12-13 13:50:35 +0100

    Ok

--- js/index.js
+++ js/index.js
@@ -1,47 +1,50 @@
 import { FormData } from './classes/FormData.js';
 const calculateAndSplit = (formData) => {
-    const result = CalculateTip(formData.sum, isNaN(formData.tip) ? 0 : formData.tip, formData.numberOfFriends);
-    displaySum(result.total());
+  const result = CalculateTip(
+    formData.sum,
+    isNaN(formData.tip) ? 0 : formData.tip,
+    formData.numberOfFriends
+  );
+  displaySum(result.total());
 };
 const CalculateTip = (sum, tip, friends) => {
-    const calculations = {
-        sum: sum,
-        tip: isNaN(tip) ? 0 : tip,
-        numberOfFriends: friends,
-        total: () => {
-            const tipInValue = sum * tip;
-            const totalWithTip = sum + tipInValue;
-            return totalWithTip / friends;
-        },
-    };
-    return calculations;
+  const calculations = {
+    sum: sum,
+    tip: isNaN(tip) ? 0 : tip,
+    numberOfFriends: friends,
+    total: () => {
+      const tipInValue = sum * tip;
+      const totalWithTip = sum + tipInValue;
+      return totalWithTip / friends;
+    },
+  };
+  return calculations;
 };
 const displaySum = (sum) => {
-    if (sum) {
-        document.getElementById('friendSum').textContent = sum.toString();
-    }
-    showHandler();
-    document.getElementById('showSum').addEventListener('click', () => {
-        location.reload();
-    });
+  if (sum) {
+    document.getElementById('friendSum').textContent = sum.toString();
+  }
+  showHandler();
+  document.getElementById('showSum').addEventListener('click', () => {
+    location.reload();
+  });
 };
 const showHandler = () => {
-    const formElements = document.querySelectorAll('.form');
-    formElements.forEach((element) => {
-        element.classList.toggle('hide');
-    });
+  const formElements = document.querySelectorAll('.form');
+  formElements.forEach((element) => {
+    element.classList.toggle('hide');
+  });
 };
 const submitForm = () => {
-    try {
-        const form = new FormData('.form__input');
-        calculateAndSplit(form);
-    }
-    catch (error) {
-        displaySum(error.message);
-    }
+  try {
+    const form = new FormData('.form__input');
+    calculateAndSplit(form);
+  } catch (error) {
+    displaySum(error.message);
+  }
 };
 const submitButton = document.getElementById('calculateButton');
 submitButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    submitForm();
+  event.preventDefault();
+  submitForm();
 });
