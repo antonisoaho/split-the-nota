0cf268cb4251b4c1d585b1e75fd7ce9223f498b2
antonisoaho <isoahoanton@gmail.com>
2023-12-14 11:40:34 +0100

    Factorizing

--- ts/script.ts
+++ ts/script.ts
@@ -1,33 +1,24 @@
-document
-  .getElementById('calculateButton')
-  .addEventListener('click', function () {
-    const sum: number = Number(
-      (<HTMLInputElement>document.getElementById('sum')).value
-    );
-    const tip: number = Number(
-      (<HTMLInputElement>document.getElementById('tip')).value
-    );
-    const numberOfFriends: number = Number(
-      (<HTMLInputElement>document.getElementById('numberOfFriends')).value
-    );
+document.getElementById('calculateButton').addEventListener('click', function () {
+  const sum: number = Number((<HTMLInputElement>document.getElementById('sum')).value);
+  const tip: number = Number((<HTMLInputElement>document.getElementById('tip')).value);
+  const numberOfFriends: number = Number(
+    (<HTMLInputElement>document.getElementById('numberOfFriends')).value
+  );
 
-    const calculatedTip: number = calculateTip(sum, tip);
-    const sumDivided: number = (sum + calculatedTip) / numberOfFriends;
+  const calculatedTip: number = calculateTip(sum, tip);
+  const sumDivided: number = (sum + calculatedTip) / numberOfFriends;
 
-    showDividedSum(sumDivided);
-  });
+  showDividedSum(sumDivided);
+});
 
 function calculateTip(sum: number, tip: number): number {
   return sum * tip;
 }
 
 function showDividedSum(sum: number): void {
-  const formElements: NodeListOf<HTMLElement> =
-    document.querySelectorAll('.form');
+  const formElements: NodeListOf<HTMLElement> = document.querySelectorAll('.form');
   formElements.forEach((form) => form.classList.toggle('hide'));
   document.getElementById('friendSum').innerHTML = sum + ' kr';
 }
 
-document
-  .getElementById('showSum')
-  .addEventListener('click', () => calculateTip(120, 20));
+document.getElementById('showSum').addEventListener('click', () => calculateTip(120, 20));
